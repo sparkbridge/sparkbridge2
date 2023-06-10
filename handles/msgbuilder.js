bd44cbda2438b582441e55a9a963d8ba8b6db3c2
Lition802 <dreamgqf@163.com>
2023-06-10 23:02:16 +0800

    添加消息构造器和包构造器

---
+++ handles/msgbuilder.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+class Builder {
+    static img(file) {
+        if (typeof file === 'string' && exists(file)) file = fs.readFileSync(file)
+        if (file instanceof Buffer) file = `base64://${file.toString('base64')}`
+        return { type: 'image', data: { file: file, subType: 0 } }
+    }
+    static at(qid) {
+        return { type: "at", data: { "qq": qid } };
+
+    }
+    static text(raw) {
+        return { type: 'text', data: { text: raw } };
+    }
+}
+
+
+module.exports = Builder;
