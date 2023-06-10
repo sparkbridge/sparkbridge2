1190b22695cff34ac570b3776a8f22739dd32e1c
Lition802 <dreamgqf@163.com>
2023-06-10 21:06:14 +0800

    修复一些问题

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SparkBridge2
 
 coming soon....
+
+未完成部分
+ - 插件按顺序加载
+ - websocket解包
+ - 全局注册命令
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 /// <reference path="c:\Users\zhouying\Documents/dts/helperlib/src/index.d.ts"/> 
 
 ll.registerPlugin(
-    /* name */ "sparkbridge",
+    /* name */ "sparkbridge2",
     /* introduction */ "a qq bot system",
     /* version */ [2,0,1]
 ); 
@@ -10,5 +10,38 @@ ll.registerPlugin(
 
 const ME = require('./package.json');
 const JSON5 = require('json5');
-const winston = require('winston');
+const path = require('path');
+const fhelper = require('./handles/file');
+const Spark = require('./spark');
+const lg = require('./handles/logger');
+const PLUGIN_ROOT_DIR = './plugins/nodejs/sparkbridge2';
+const PLUGIN_DATA_DIR = './plugins/sparkbridge2';
+if(fhelper.exists(PLUGIN_DATA_DIR) == false) fhelper.mkdir(PLUGIN_DATA_DIR);
+console.log(fhelper.read(PLUGIN_ROOT_DIR+'/logo.txt'));
 
+let ROOT_FILE_HELPER =new fhelper.FileObj('base');
+ROOT_FILE_HELPER.initFile('config.json',{target:"ws://127.0.0.1:8080",qid:114514,pwd:''});
+let RAW_CONFIG = ROOT_FILE_HELPER.getFile('config.json');
+const CONFIG = JSON.parse(RAW_CONFIG);
+
+global.spark = new Spark(CONFIG.target,CONFIG.qid,CONFIG.pwd);
+
+const logger = lg.getLogger('sparkbridge2')
+
+mc.listen('onServerStarted', () => {
+    const PLUGINS_PATH = path.join(__dirname, 'plugins\\');
+    const plugins_list = fhelper.listdir(PLUGINS_PATH);
+    const laodPlugin = (_name) =>{
+        try {
+            let pl_obj = require('./plugins/'+_name);
+            let pl_info = require('./plugins/'+_name+"/spark.json")
+            logger.info(`加载 ${pl_info.name}`);
+            //pl_obj.onStart(_adapter);
+            logger.info(`${pl_info.name} 加载完成，作者：${pl_info.author}`);
+        } catch (err) {
+            console.log(err);
+            logger.error(`插件 ${_name} 加载失败`);
+        }
+    }
+    laodPlugin('bridgebase')
+})
