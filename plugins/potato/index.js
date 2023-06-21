f9775e1333960a33838fb6db64f993c9f07adafc
Daniel Lin <53430542+DanielToyama@users.noreply.github.com>
2023-06-21 09:43:34 +0800

    为示例插件“Potato”增加群检测
    Rt

--- plugins/potato/index.js
+++ plugins/potato/index.js
@@ -4,7 +4,9 @@ spark.QClient.on('bot.online', () => {
 });
 
 spark.on('message.group.normal', (e, reply) => {
-    const { raw_message } = e;
+    const { raw_message,group_id } = e;
+    if (group_id !== spark.mc.config.group) return;
+    //此处判断是否属于配置文件群
     if (raw_message == '土豆')
         reply(msgbuilder.img('https://s1.ax1x.com/2023/04/11/ppqjoo8.md.jpg'))
 })
