877360fdde9a4708ded566663efad0a74984501b
lition <dreamgqf@163.com>
2023-07-24 19:36:56 +0800

    修复时间显示

--- llapi.js
+++ llapi.js
@@ -1,4 +1,6 @@
 // LiteLoader-AIDS automatic generated
-/// <reference path="d://dts/helperlib/src/index.d.ts"/> 
+/// <reference path="c:\Users\dream\Documents/dts/HelperLib-master/src/index.d.ts"/> 
+
+
 
 // 单独开发请在这里引入你们的 API 地址
