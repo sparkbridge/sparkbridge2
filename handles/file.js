c45b7010623e708cd174e39d7d181a217bf4bfcb
DanielToyama <jswnlinyu@Outlook.com>
2024-05-23 00:09:22 +0800

    兼容Liteloader（LL2）

--- handles/file.js
+++ handles/file.js
@@ -1,41 +1,47 @@
 const fs = require('fs');
 var PLUGIN_DATA_DIR;
-if (typeof mc !== 'undefined'){
-    PLUGIN_DATA_DIR = './plugins/sparkbridge2/serverdata'
-}else{
+if (typeof mc !== 'undefined') {
+    if (!exists('./plugins/LeviLamina')) {
+        PLUGIN_DATA_DIR = './plugins/nodejs/sparkbridge2/serverdata'
+
+    }
+    else {
+        PLUGIN_DATA_DIR = './plugins/sparkbridge2/serverdata'
+    }
+} else {
     PLUGIN_DATA_DIR = './testdata'
 }
 
 
-const JSON5  = require('json5');
-class FileObj{
+const JSON5 = require('json5');
+class FileObj {
     pname;
-    constructor(plugin_name){
+    constructor(plugin_name) {
         this.pname = plugin_name;
-        if(exists(PLUGIN_DATA_DIR+'/'+this.pname) == false){
-            mkdir(PLUGIN_DATA_DIR+'/'+this.pname);
+        if (exists(PLUGIN_DATA_DIR + '/' + this.pname) == false) {
+            mkdir(PLUGIN_DATA_DIR + '/' + this.pname);
         }
     }
-    initFile(fname,init_obj,json = JSON){
-        if(exists(PLUGIN_DATA_DIR+'/'+this.pname+'/'+fname) == false)
-            writeTo(PLUGIN_DATA_DIR+'/'+this.pname+'/'+fname,json.stringify(init_obj,null,4));
+    initFile(fname, init_obj, json = JSON) {
+        if (exists(PLUGIN_DATA_DIR + '/' + this.pname + '/' + fname) == false)
+            writeTo(PLUGIN_DATA_DIR + '/' + this.pname + '/' + fname, json.stringify(init_obj, null, 4));
     }
-    getFile(fname){
-        if(exists(PLUGIN_DATA_DIR+'/'+this.pname) == false){
+    getFile(fname) {
+        if (exists(PLUGIN_DATA_DIR + '/' + this.pname) == false) {
             return null;
-        }else{
-            return read(PLUGIN_DATA_DIR+'/'+this.pname+'/'+fname)
+        } else {
+            return read(PLUGIN_DATA_DIR + '/' + this.pname + '/' + fname)
         }
     }
-    getBuffer(fname){
-        if(exists(PLUGIN_DATA_DIR+'/'+this.pname) == false){
+    getBuffer(fname) {
+        if (exists(PLUGIN_DATA_DIR + '/' + this.pname) == false) {
             return null;
-        }else{
-            return fs.readFileSync(PLUGIN_DATA_DIR+'/'+this.pname+'/'+fname)
+        } else {
+            return fs.readFileSync(PLUGIN_DATA_DIR + '/' + this.pname + '/' + fname)
         }
     }
-    updateFile(fname,data_obj,json= JSON){
-        writeTo(PLUGIN_DATA_DIR+'/'+this.pname+'/'+fname,json.stringify(data_obj,null,4));
+    updateFile(fname, data_obj, json = JSON) {
+        writeTo(PLUGIN_DATA_DIR + '/' + this.pname + '/' + fname, json.stringify(data_obj, null, 4));
     }
 }
 
@@ -44,39 +50,39 @@ class FileObj{
 function exists(pt) {
     try {
         return fs.existsSync(pt);
-    } catch(e) {console.log(e) }
+    } catch (e) { console.log(e) }
 }
 
 function writeTo(pt, raw) {
     try {
-        fs.writeFileSync(pt, raw,{encoding:'utf-8'});
-    } catch(e) {console.log(e) }
+        fs.writeFileSync(pt, raw, { encoding: 'utf-8' });
+    } catch (e) { console.log(e) }
 }
 
 function mkdir(pt) {
     try {
         fs.mkdirSync(pt);
-    } catch(e) {console.log(e) }
+    } catch (e) { console.log(e) }
 }
 
 function copy(pt, npt) {
     try {
         fs.copyFileSync(pt, npt);
-    } catch(e) {console.log(e) }
+    } catch (e) { console.log(e) }
 }
 
 function read(pt) {
     try {
-        return fs.readFileSync(pt,{encoding:'utf-8'});
-    } catch(e) {console.log(e) }
+        return fs.readFileSync(pt, { encoding: 'utf-8' });
+    } catch (e) { console.log(e) }
 }
 
 function listdir(pt) {
     try {
         return fs.readdirSync(pt);
-    } catch(e) {console.log(e) }
+    } catch (e) { console.log(e) }
 }
 
 module.exports = {
-    exists, writeTo, mkdir, copy, read, listdir ,FileObj
+    exists, writeTo, mkdir, copy, read, listdir, FileObj
 }
