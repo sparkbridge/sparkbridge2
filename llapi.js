0379b8c967bc86c4e035854b5d1e0503a6d821f4
pingguo114514 <1135636981@qq.com>
2025-05-04 22:07:05 +0800

    优化导出API，新增导出packbuilder，优化导入和使用流程

--- llapi.js
+++ llapi.js
@@ -1,5 +1,27 @@
-// LiteLoader-AIDS automatic generated
-/// <reference path="c:\Users\dream\Documents\llaids/dts/helperlib/src/index.d.ts"/> 
-
-
-// 单独开发请在这里引入你们的 API 地址
+const CallEvent = ll.imports("SparkAPI", "callCustomEvent");
+const GetEventID = ll.imports("SparkAPI", "GetEventID");
+function importClass(name) {
+    let res = {}, list = ll.imports("SparkAPI", name)();
+    list.forEach(funcname => res[funcname] = ll.imports("SparkAPI", `${name}.${funcname}`));
+    return res;
+}
+const msgbuilder = importClass('msgbuilder'), packbuilder = importClass('packbuilder');
+module.exports = {
+    spark: {
+        mc: {
+            config: ll.imports("SparkAPI", "GetInfo")('spark.mc.config')
+        },
+        on: (event, callback) => {
+            let eventId = GetEventID();
+            ll.exports(callback, event, eventId);
+            CallEvent(event, eventId);
+            return eventId;
+        },
+        QClient: {
+            sendGroupMsg: ll.imports("SparkAPI", "sendGroupMsg"),
+            sendWSPack: ll.imports("SparkAPI", "sendWSPack"),
+        },
+    },
+    msgbuilder: msgbuilder,
+    packbuilder: packbuilder
+}
